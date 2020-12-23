086fd3a1df51c1ccfa43e02ea694b7ae28c81511
muhammedhassanm <31989237+muhammedhassanm@users.noreply.github.com>
2020-12-23 21:53:02 +0530

    Update video_frames.py

--- Sample_Python/video_frames.py
+++ Sample_Python/video_frames.py
@@ -7,16 +7,16 @@ Created on Tue Feb 18 18:47:47 2020
 
 
 import cv2
-vidcap = cv2.VideoCapture('video.mp4')
+vidcap = cv2.VideoCapture('C:/Users/100119/Pictures/AKARSH ZF/SOCKS_DATA/drive-download-20201219T094324Z-001/1_ (9).mp4')
 def getFrame(sec):
     vidcap.set(cv2.CAP_PROP_POS_MSEC,sec*1000)
     hasFrames,image = vidcap.read()
     if hasFrames:
-        cv2.imwrite("image"+str(count)+".jpg", image)     # save frame as JPG file
+        cv2.imwrite('C:/Users/100119/Pictures/AKARSH ZF/SOCKS_DATA/VIDEO_FRAMES/image_'+str(count)+".jpg", image)     # save frame as JPG file
     return hasFrames
 sec = 0
 frameRate = 0.5 #//it will capture image in each 0.5 second
-count=1
+#count=1
 success = getFrame(sec)
 while success:
     count = count + 1
